056ebdb4717f3eba3f4c91136f20dfe0553c6254
Nikita <nikita.c04@mail.ru>
2021-12-22 00:52:12 +0300

    start

---
+++ src/main/java/com/aqulasoft/generationplugin/GenerationPlugin.java
@@ -0,0 +1,17 @@
+package com.aqulasoft.generationplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GenerationPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
